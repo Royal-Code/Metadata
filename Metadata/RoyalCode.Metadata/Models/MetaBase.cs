1d3450e36d328a6f351d05b0cf0af12d5b66e5f5
Glauco <eglauko@hotmail.com>
2021-01-14 00:42:14 -0300

    ok

--- Metadata/RoyalCode.Metadata/Models/MetaBase.cs
+++ Metadata/RoyalCode.Metadata/Models/MetaBase.cs
@@ -70,9 +70,21 @@ namespace RoyalCode.Metadata.Models
             return settings.ContainsKey(typeof(TSettings));
         }
 
+        /// <summary>
+        /// Verifica se existe algum setting com nome específico configurado neste modelo.
+        /// </summary>
+        /// <param name="name">Nome do settings.</param>
+        /// <typeparam name="TSettings">Tipo, classe, do Settings.</typeparam>
+        /// <returns>Se existe o Settings com o nome para este modelo.</returns>
         public virtual bool HasNamedSettings<TSettings>(string name) 
         {
+            if (settings.ContainsKey(typeof(NamedSettings<TSettings>)))
+            {
+                var named = (NamedSettings<TSettings>) settings[typeof(NamedSettings<TSettings>)];
+                return named.HasSettings(name);
+            }
 
+            return false;
         }
 
         /// <summary>
@@ -87,6 +99,29 @@ namespace RoyalCode.Metadata.Models
         }
 
         /// <summary>
+        /// Adiciona um Settings com um nome específico ao modelo.
+        /// </summary>
+        /// <param name="value">Instância, objeto, do Settings.</param>
+        /// <param name="name">Nome do settings.</param>
+        /// <typeparam name="TSettings">Settings a ser adicionado.</typeparam>
+        /// <exception cref="ArgumentException">Se já existir um tipo, classe, de Settings com o mesmo nome no modelo.</exception>
+        public virtual void AddNamedSettings<TSettings>(TSettings value, string name)
+        {
+            NamedSettings<TSettings> named;
+            if (settings.ContainsKey(typeof(NamedSettings<TSettings>)))
+            {
+                named = (NamedSettings<TSettings>) settings[typeof(NamedSettings<TSettings>)];
+            }
+            else
+            {
+                named = new NamedSettings<TSettings>();
+                settings.Add(typeof(NamedSettings<TSettings>), named);
+            }
+
+            named.AddSettings(value, name);
+        }
+
+        /// <summary>
         /// Obtém o Setting pelo tipo, classe.
         /// </summary>
         /// <typeparam name="TSettings">Tipo, classe, de Settings a ser obtido.</typeparam>
@@ -94,6 +129,23 @@ namespace RoyalCode.Metadata.Models
         public virtual TSettings GetSettings<TSettings>()
         {
             return (TSettings)settings[typeof(TSettings)];
+        }
+
+        /// <summary>
+        /// Obtém o Setting com um nome específico pelo tipo, classe.
+        /// </summary>
+        /// <param name="name">Nome do settings.</param>
+        /// <typeparam name="TSettings">Tipo, classe, de Settings a ser obtido.</typeparam>
+        /// <returns>A instância de Settings ou nulo.</returns>
+        public virtual TSettings GetNamedSettings<TSettings>(string name)
+        {
+            if (settings.ContainsKey(typeof(NamedSettings<TSettings>)))
+            {
+                var named = (NamedSettings<TSettings>) settings[typeof(NamedSettings<TSettings>)];
+                return named.GetSettings(name);
+            }
+
+            return default;
         }
 
         /// <summary>
@@ -110,6 +162,20 @@ namespace RoyalCode.Metadata.Models
         }
 
         /// <summary>
+        /// Tenta obtém o settings com nome específico se ele existir.
+        /// </summary>
+        /// <param name="name">Nome do settings.</param>
+        /// <param name="settings">Saída como instância de Settings, ou default se não existir.</param>
+        /// <typeparam name="TSettings">Tipo, classe, de Settings a ser obtido.</typeparam>
+        /// <returns>Verdadeiro se existir, falso caso contrário.</returns>
+        public virtual bool TryGetNamedSettings<TSettings>(string name, out TSettings settings)
+        {
+            var result = HasNamedSettings<TSettings>(name);
+            settings = result ? GetNamedSettings<TSettings>(name) : default;
+            return result;
+        }
+
+        /// <summary>
         /// Obtém ou cria um Settings para este modelo.
         /// </summary>
         /// <typeparam name="TSettings">Tipo, classe, de Settings a ser obtido.</typeparam>
@@ -118,9 +184,24 @@ namespace RoyalCode.Metadata.Models
             where TSettings : new()
         {
             if (!HasSettings<TSettings>())
-                AddSettings(new TSettings());
+                AddSettings(CreateSettings<TSettings>());
 
             return GetSettings<TSettings>();
+        }
+
+        /// <summary>
+        /// Obtém ou cria um Settings com nome específico para este modelo.
+        /// </summary>
+        /// <param name="name">Nome do settings.</param>
+        /// <typeparam name="TSettings">Tipo, classe, de Settings a ser obtido.</typeparam>
+        /// <returns>A instância de Settings.</returns>
+        public virtual TSettings GetOrCreateNamedSettings<TSettings>(string name)
+            where TSettings : new()
+        {
+            if (!HasNamedSettings<TSettings>(name))
+                AddNamedSettings(CreateSettings<TSettings>(), name);
+
+            return GetNamedSettings<TSettings>(name);
         }
 
         /// <summary>
@@ -137,12 +218,45 @@ namespace RoyalCode.Metadata.Models
             return GetSettings<TSettings>();
         }
 
+        /// <summary>
+        /// btém ou cria um Settings com nome específico para este modelo.
+        /// </summary>
+        /// <param name="name">Nome do settings.</param>
+        /// <param name="factory">Função para criar um <typeparamref name="TSettings"/>.</param>
+        /// <typeparam name="TSettings">Tipo, classe, de Settings a ser obtido.</typeparam>
+        /// <returns>A instância de Settings.</returns>
+        public virtual TSettings GetOrCreateNamedSettings<TSettings>(string name, Func<TSettings> factory)
+        {
+            if (!HasNamedSettings<TSettings>(name))
+                AddNamedSettings(factory(), name);
+
+            return GetNamedSettings<TSettings>(name);
+        }
+
         protected virtual TSettings CreateSettings<TSettings>()
             where TSettings : new()
         {
             return new TSettings();
         }
 
-        private class NamedSettings<TSettings> { }
+        private class NamedSettings<TSettings>
+        {
+            private readonly Dictionary<string, TSettings> settings = new();
+            
+            public bool HasSettings(string name)
+            {
+                return settings.ContainsKey(name);
+            }
+
+            public void AddSettings(TSettings value, string name)
+            {
+                settings.Add(name, value);
+            }
+
+            public TSettings GetSettings(string name)
+            {
+                return settings[name];
+            }
+        }
     }
 }
